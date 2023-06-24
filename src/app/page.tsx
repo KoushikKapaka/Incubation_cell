4ab5b9bd54667b431149c49c7c60c20232cc14d4
kapakakoushik <koushik500062@gmail.com>
2023-06-24 16:40:42 +0530

    second commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,138 @@
-import Image from 'next/image'
+import ImageFallback from "@/layouts/components/ImageFallback";
+import { getListPage } from "@/lib/contentParser";
+import { markdownify } from "@/lib/utils/textConverter";
+import CallToAction from "@/partials/CallToAction";
+import SeoMeta from "@/partials/SeoMeta";
+import Testimonials from "@/partials/Testimonials";
+import { Button, Feature } from "@/types";
+import { FaCheck } from "react-icons/fa/index.js";
 
-export default function Home() {
+const Home = () => {
+  const homepage = getListPage("_index.md");
+  const testimonial = getListPage("sections/testimonial.md");
+  const callToAction = getListPage("sections/call-to-action.md");
+  const { frontmatter } = homepage;
+  const {
+    banner,
+    features,
+  }: {
+    banner: { title: string; image: string; content?: string; button?: Button };
+    features: Feature[];
+  } = frontmatter;
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <>
+      <SeoMeta />
+      <section className="section pt-14">
+        <div className="container">
+          <div className="row justify-center">
+            {banner.image && (
+              <div className="col-12">
+                <ImageFallback
+                  src={banner.image}
+                  className="mx-auto"
+                  width="800"
+                  height="420"
+                  alt="banner image"
+                  priority
+                />
+              </div>
+            )}
+          </div>
         </div>
-      </div>
+      </section>
+      <section className="section pt-14">
+        <div className="container">
+          <div className="row justify-center">
+            <div className="mb-16 text-center lg:col-7">
+              <h1
+                className="mb-4"
+                dangerouslySetInnerHTML={markdownify(banner.title)}
+              />
+              <p
+                className="mb-8"
+                dangerouslySetInnerHTML={markdownify(banner.content ?? "")}
+              />
+              {banner.button!.enable && (
+                <a className="btn btn-primary" href={banner.button!.link}>
+                  {banner.button!.label}
+                </a>
+              )}
+            </div>
+            {banner.image && (
+              <div className="col-12">
+                <ImageFallback
+                  src={banner.image}
+                  className="mx-auto"
+                  width="800"
+                  height="420"
+                  alt="banner image"
+                  priority
+                />
+              </div>
+            )}
+          </div>
+        </div>
+      </section>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+      {features.map((feature, index: number) => (
+        <section
+          key={index}
+          className={`section-sm ${index % 2 === 0 && "bg-gradient"}`}
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+          <div className="container">
+            <div className="row items-center justify-between">
+              <div
+                className={`mb:md-0 mb-6 md:col-5 ${
+                  index % 2 !== 0 && "md:order-2"
+                }`}
+              >
+                <ImageFallback
+                  src={feature.image}
+                  height={480}
+                  width={520}
+                  alt={feature.title}
+                />
+              </div>
+              <div
+                className={`md:col-7 lg:col-6 ${
+                  index % 2 !== 0 && "md:order-1"
+                }`}
+              >
+                <h2
+                  className="mb-4"
+                  dangerouslySetInnerHTML={markdownify(feature.title)}
+                />
+                <p
+                  className="mb-8 text-lg"
+                  dangerouslySetInnerHTML={markdownify(feature.content)}
+                />
+                <ul>
+                  {feature.bulletpoints.map((bullet: string) => (
+                    <li className="relative mb-4 pl-6" key={bullet}>
+                      <FaCheck className={"absolute left-0 top-1.5"} />
+                      <span dangerouslySetInnerHTML={markdownify(bullet)} />
+                    </li>
+                  ))}
+                </ul>
+                {feature.button.enable && (
+                  <a
+                    className="btn btn-primary mt-5"
+                    href={feature.button.link}
+                  >
+                    {feature.button.label}
+                  </a>
+                )}
+              </div>
+            </div>
+          </div>
+        </section>
+      ))}
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+      <Testimonials data={testimonial} />
+      <CallToAction data={callToAction} />
+    </>
+  );
+};
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
-}
+export default Home;
